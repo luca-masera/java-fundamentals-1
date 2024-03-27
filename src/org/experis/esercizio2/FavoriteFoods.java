a68207d882f29712a2548097684bddd778fb2087
luca-masera <lucamasera1995@gmail.com>
2024-03-27 15:30:10 +0100

    Correction exercise 2 and initial exercise 3

--- src/org/experis/esercizio2/FavoriteFoods.java
+++ src/org/experis/esercizio2/FavoriteFoods.java
@@ -6,7 +6,7 @@ public class FavoriteFoods {
         System.out.println("Il numero totale degli elementi è:" + foods.length);
         System.out.println("Il mio cibo preferito è la:" + foods[0]);
         System.out.println("Il mio cibo semi-preferito è:" + foods[foods.length-1]);
-        System.out.println("Il cibo di mezza classifica è:" + foods[2]);
+        System.out.println("Il cibo di mezza classifica è:" + foods[foods.length/2]);
 
     }
 }
---
+++ src/org/experis/esercizio3/FizzBuzz.java
@@ -0,0 +1,10 @@
+package org.experis.esercizio3;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
