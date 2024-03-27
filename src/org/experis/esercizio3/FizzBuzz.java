379408a61d0e6b50f32ce886f0b9f6fbe1923bd0
luca-masera <lucamasera1995@gmail.com>
2024-03-27 15:54:49 +0100

    exercise 3

--- src/org/experis/esercizio3/FizzBuzz.java
+++ src/org/experis/esercizio3/FizzBuzz.java
@@ -2,8 +2,18 @@ package org.experis.esercizio3;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i < 101; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+
+            if (i%3 == 0 && i%5 == 0){
+                System.out.println("Fizz");
+            } else if (i%3 == 0) {
+                System.out.println("Buzz");
+            } else if (i%5 == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(i);
+            }
+            //System.out.println(i);
         }
 
     }
