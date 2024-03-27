5cde2fa281637330de250bea38c49fb5e73d4dc1
luca-masera <lucamasera1995@gmail.com>
2024-03-27 14:30:24 +0100

    ask age and kilometers

--- src/org/experis/esercizio1/TrainTicket.java
+++ src/org/experis/esercizio1/TrainTicket.java
@@ -1,2 +1,19 @@
-package org.experis.esercizio1;public class TrainTicket {
+package org.experis.esercizio1;
+
+import java.util.Scanner;
+
+public class TrainTicket {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //Chiedere al passeggero il numero di km che vuole percorrere
+        System.out.println("How many kilometers do you want to travel?");
+        int kilometers = scan.nextInt();
+
+        //Chiedere al passeggero l'età
+        System.out.println("How old are you?");
+        int age = scan.nextInt();
+
+
+    }
 }
