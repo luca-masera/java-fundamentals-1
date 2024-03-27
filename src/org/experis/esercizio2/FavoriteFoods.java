fbf33f6fea5813552847822771a85b0bfa01e8c1
luca-masera <lucamasera1995@gmail.com>
2024-03-27 15:09:48 +0100

    Exercise 2, my favorite foods

---
+++ src/org/experis/esercizio2/FavoriteFoods.java
@@ -0,0 +1,12 @@
+package org.experis.esercizio2;
+
+public class FavoriteFoods {
+    public static void main(String[] args) {
+        String[] foods = {"pizza", "pasta", "arancia", "cioccolato", "carne", "polenta"};
+        System.out.println("Il numero totale degli elementi è:" + foods.length);
+        System.out.println("Il mio cibo preferito è la:" + foods[0]);
+        System.out.println("Il mio cibo semi-preferito è:" + foods[foods.length-1]);
+        System.out.println("Il cibo di mezza classifica è:" + foods[2]);
+
+    }
+}
---
+++ src/org/experis/esercizio2/README.md
@@ -0,0 +1,6 @@
+Nel programma inizializzate un array (con i metodi visti a lezione) con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi)
+Poi stampate a video:
+la lunghezza della classifica
+il vostro cibo top (prima posizione della classifica)
+il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+il cibo di mezza classifica, cioè che si trova nella posizione mediana
